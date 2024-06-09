cb4bba761e788201512e9657775e6e5325b80f46
Benjamin_Vincent <xywenjie@outlook.com>
2024-06-09 10:40:58 +0800

    Hilla基本框架

--- frontend/themes/index.tsx
+++ frontend/themes/index.tsx
@@ -1,17 +1,27 @@
-import {useMemo} from 'react';
+import {useMemo} from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import {createTheme,ThemeProvider as MuiThemeProvider} from "@mui/material";
+import {palette} from "Frontend/themes/palette";
+import PropTypes from "prop-types";
 
 type Props = {
     children:React.ReactNode;
-};
-
-export default function ThemeProvider({children} : Props){
-    const memoizedValue = useMemo(
-        () => ({
-            palette:palette('light'),
-            shadows:shadows('light'),
-            customShadows:customShadows('light'),
-            shape:{borderRadius:8},
-            typography,
-        }),[]
-    );
 }
+
+export default function ThemeProvider({children}:Props){
+    const memoizedValue = useMemo(()=>({
+        palette:palette(),
+    }),[]);
+
+    const theme = createTheme(memoizedValue);
+
+    theme.components = overrides(theme);
+    return (<MuiThemeProvider theme={theme}>
+        <CssBaseline/>
+        {children}
+    </MuiThemeProvider>);
+}
+
+ThemeProvider.prototype = {
+    children:PropTypes.node,
+};
